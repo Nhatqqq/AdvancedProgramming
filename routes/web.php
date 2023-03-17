40bac8236fd1f00ccf5ac8187aaff7e10e2d8d40
tuan <vanminh291974@gmail.com>
2023-03-17 18:09:28 +0700

    login
    login

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::prefix('logout')->group(function () {
+    Route::post('/',[AuthController::class,'postLogout'])->name('auth.logout');
+});
